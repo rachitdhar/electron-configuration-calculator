53dd910b0ef49f5e14d96d5d2e29ad3a71a18136
rachitdhar <75770595+rachitdhar@users.noreply.github.com>
2024-07-12 12:16:11 +0530

    added project files for app

---
+++ VB Electron Configuration/Form1.Designer.vb
@@ -0,0 +1,452 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.no_e = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ok = New System.Windows.Forms.Button()
+        Me.s1 = New System.Windows.Forms.Label()
+        Me.s2 = New System.Windows.Forms.Label()
+        Me.s4 = New System.Windows.Forms.Label()
+        Me.s6 = New System.Windows.Forms.Label()
+        Me.s9 = New System.Windows.Forms.Label()
+        Me.s12 = New System.Windows.Forms.Label()
+        Me.s16 = New System.Windows.Forms.Label()
+        Me.s20 = New System.Windows.Forms.Label()
+        Me.s25 = New System.Windows.Forms.Label()
+        Me.s3 = New System.Windows.Forms.Label()
+        Me.s5 = New System.Windows.Forms.Label()
+        Me.s8 = New System.Windows.Forms.Label()
+        Me.s11 = New System.Windows.Forms.Label()
+        Me.s15 = New System.Windows.Forms.Label()
+        Me.s19 = New System.Windows.Forms.Label()
+        Me.s24 = New System.Windows.Forms.Label()
+        Me.s7 = New System.Windows.Forms.Label()
+        Me.s10 = New System.Windows.Forms.Label()
+        Me.s14 = New System.Windows.Forms.Label()
+        Me.s18 = New System.Windows.Forms.Label()
+        Me.s23 = New System.Windows.Forms.Label()
+        Me.s13 = New System.Windows.Forms.Label()
+        Me.s17 = New System.Windows.Forms.Label()
+        Me.s22 = New System.Windows.Forms.Label()
+        Me.s21 = New System.Windows.Forms.Label()
+        Me.ec = New System.Windows.Forms.RichTextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(144, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Write Electron or Atomic No.:"
+        '
+        'no_e
+        '
+        Me.no_e.Location = New System.Drawing.Point(15, 35)
+        Me.no_e.Name = "no_e"
+        Me.no_e.Size = New System.Drawing.Size(100, 20)
+        Me.no_e.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(83, 71)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(112, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Electron Configuraton:"
+        '
+        'ok
+        '
+        Me.ok.Location = New System.Drawing.Point(197, 32)
+        Me.ok.Name = "ok"
+        Me.ok.Size = New System.Drawing.Size(75, 23)
+        Me.ok.TabIndex = 4
+        Me.ok.Text = "OK"
+        Me.ok.UseVisualStyleBackColor = True
+        '
+        's1
+        '
+        Me.s1.AccessibleName = "1s"
+        Me.s1.AutoSize = True
+        Me.s1.Location = New System.Drawing.Point(304, 9)
+        Me.s1.Name = "s1"
+        Me.s1.Size = New System.Drawing.Size(13, 13)
+        Me.s1.TabIndex = 5
+        Me.s1.Tag = "2"
+        Me.s1.Text = "0"
+        '
+        's2
+        '
+        Me.s2.AccessibleName = "2s"
+        Me.s2.AutoSize = True
+        Me.s2.Location = New System.Drawing.Point(304, 37)
+        Me.s2.Name = "s2"
+        Me.s2.Size = New System.Drawing.Size(13, 13)
+        Me.s2.TabIndex = 6
+        Me.s2.Tag = "2"
+        Me.s2.Text = "0"
+        '
+        's4
+        '
+        Me.s4.AccessibleName = "3s"
+        Me.s4.AutoSize = True
+        Me.s4.Location = New System.Drawing.Point(304, 62)
+        Me.s4.Name = "s4"
+        Me.s4.Size = New System.Drawing.Size(13, 13)
+        Me.s4.TabIndex = 7
+        Me.s4.Tag = "2"
+        Me.s4.Text = "0"
+        '
+        's6
+        '
+        Me.s6.AccessibleName = "4s"
+        Me.s6.AutoSize = True
+        Me.s6.Location = New System.Drawing.Point(304, 89)
+        Me.s6.Name = "s6"
+        Me.s6.Size = New System.Drawing.Size(13, 13)
+        Me.s6.TabIndex = 8
+        Me.s6.Tag = "2"
+        Me.s6.Text = "0"
+        '
+        's9
+        '
+        Me.s9.AccessibleName = "5s"
+        Me.s9.AutoSize = True
+        Me.s9.Location = New System.Drawing.Point(304, 118)
+        Me.s9.Name = "s9"
+        Me.s9.Size = New System.Drawing.Size(13, 13)
+        Me.s9.TabIndex = 9
+        Me.s9.Tag = "2"
+        Me.s9.Text = "0"
+        '
+        's12
+        '
+        Me.s12.AccessibleName = "6s"
+        Me.s12.AutoSize = True
+        Me.s12.Location = New System.Drawing.Point(304, 146)
+        Me.s12.Name = "s12"
+        Me.s12.Size = New System.Drawing.Size(13, 13)
+        Me.s12.TabIndex = 10
+        Me.s12.Tag = "2"
+        Me.s12.Text = "0"
+        '
+        's16
+        '
+        Me.s16.AccessibleName = "7s"
+        Me.s16.AutoSize = True
+        Me.s16.Location = New System.Drawing.Point(304, 173)
+        Me.s16.Name = "s16"
+        Me.s16.Size = New System.Drawing.Size(13, 13)
+        Me.s16.TabIndex = 11
+        Me.s16.Tag = "2"
+        Me.s16.Text = "0"
+        '
+        's20
+        '
+        Me.s20.AccessibleName = "8s"
+        Me.s20.AutoSize = True
+        Me.s20.Location = New System.Drawing.Point(304, 200)
+        Me.s20.Name = "s20"
+        Me.s20.Size = New System.Drawing.Size(13, 13)
+        Me.s20.TabIndex = 12
+        Me.s20.Tag = "2"
+        Me.s20.Text = "0"
+        '
+        's25
+        '
+        Me.s25.AccessibleName = "9s"
+        Me.s25.AutoSize = True
+        Me.s25.Location = New System.Drawing.Point(304, 226)
+        Me.s25.Name = "s25"
+        Me.s25.Size = New System.Drawing.Size(13, 13)
+        Me.s25.TabIndex = 13
+        Me.s25.Tag = "2"
+        Me.s25.Text = "0"
+        '
+        's3
+        '
+        Me.s3.AccessibleName = "2p"
+        Me.s3.AutoSize = True
+        Me.s3.Location = New System.Drawing.Point(349, 38)
+        Me.s3.Name = "s3"
+        Me.s3.Size = New System.Drawing.Size(13, 13)
+        Me.s3.TabIndex = 14
+        Me.s3.Tag = "6"
+        Me.s3.Text = "0"
+        '
+        's5
+        '
+        Me.s5.AccessibleName = "3p"
+        Me.s5.AutoSize = True
+        Me.s5.Location = New System.Drawing.Point(349, 62)
+        Me.s5.Name = "s5"
+        Me.s5.Size = New System.Drawing.Size(13, 13)
+        Me.s5.TabIndex = 15
+        Me.s5.Tag = "6"
+        Me.s5.Text = "0"
+        '
+        's8
+        '
+        Me.s8.AccessibleName = "4p"
+        Me.s8.AutoSize = True
+        Me.s8.Location = New System.Drawing.Point(349, 89)
+        Me.s8.Name = "s8"
+        Me.s8.Size = New System.Drawing.Size(13, 13)
+        Me.s8.TabIndex = 16
+        Me.s8.Tag = "6"
+        Me.s8.Text = "0"
+        '
+        's11
+        '
+        Me.s11.AccessibleName = "5p"
+        Me.s11.AutoSize = True
+        Me.s11.Location = New System.Drawing.Point(349, 118)
+        Me.s11.Name = "s11"
+        Me.s11.Size = New System.Drawing.Size(13, 13)
+        Me.s11.TabIndex = 17
+        Me.s11.Tag = "6"
+        Me.s11.Text = "0"
+        '
+        's15
+        '
+        Me.s15.AccessibleName = "6p"
+        Me.s15.AutoSize = True
+        Me.s15.Location = New System.Drawing.Point(349, 146)
+        Me.s15.Name = "s15"
+        Me.s15.Size = New System.Drawing.Size(13, 13)
+        Me.s15.TabIndex = 18
+        Me.s15.Tag = "6"
+        Me.s15.Text = "0"
+        '
+        's19
+        '
+        Me.s19.AccessibleName = "7p"
+        Me.s19.AutoSize = True
+        Me.s19.Location = New System.Drawing.Point(349, 173)
+        Me.s19.Name = "s19"
+        Me.s19.Size = New System.Drawing.Size(13, 13)
+        Me.s19.TabIndex = 19
+        Me.s19.Tag = "6"
+        Me.s19.Text = "0"
+        '
+        's24
+        '
+        Me.s24.AccessibleName = "8p"
+        Me.s24.AutoSize = True
+        Me.s24.Location = New System.Drawing.Point(349, 200)
+        Me.s24.Name = "s24"
+        Me.s24.Size = New System.Drawing.Size(13, 13)
+        Me.s24.TabIndex = 20
+        Me.s24.Tag = "6"
+        Me.s24.Text = "0"
+        '
+        's7
+        '
+        Me.s7.AccessibleName = "3d"
+        Me.s7.AutoSize = True
+        Me.s7.Location = New System.Drawing.Point(400, 62)
+        Me.s7.Name = "s7"
+        Me.s7.Size = New System.Drawing.Size(13, 13)
+        Me.s7.TabIndex = 21
+        Me.s7.Tag = "10"
+        Me.s7.Text = "0"
+        '
+        's10
+        '
+        Me.s10.AccessibleName = "4d"
+        Me.s10.AutoSize = True
+        Me.s10.Location = New System.Drawing.Point(400, 89)
+        Me.s10.Name = "s10"
+        Me.s10.Size = New System.Drawing.Size(13, 13)
+        Me.s10.TabIndex = 22
+        Me.s10.Tag = "10"
+        Me.s10.Text = "0"
+        '
+        's14
+        '
+        Me.s14.AccessibleName = "5d"
+        Me.s14.AutoSize = True
+        Me.s14.Location = New System.Drawing.Point(400, 118)
+        Me.s14.Name = "s14"
+        Me.s14.Size = New System.Drawing.Size(13, 13)
+        Me.s14.TabIndex = 23
+        Me.s14.Tag = "10"
+        Me.s14.Text = "0"
+        '
+        's18
+        '
+        Me.s18.AccessibleName = "6d"
+        Me.s18.AutoSize = True
+        Me.s18.Location = New System.Drawing.Point(400, 146)
+        Me.s18.Name = "s18"
+        Me.s18.Size = New System.Drawing.Size(13, 13)
+        Me.s18.TabIndex = 24
+        Me.s18.Tag = "10"
+        Me.s18.Text = "0"
+        '
+        's23
+        '
+        Me.s23.AccessibleName = "7d"
+        Me.s23.AutoSize = True
+        Me.s23.Location = New System.Drawing.Point(400, 173)
+        Me.s23.Name = "s23"
+        Me.s23.Size = New System.Drawing.Size(13, 13)
+        Me.s23.TabIndex = 25
+        Me.s23.Tag = "10"
+        Me.s23.Text = "0"
+        '
+        's13
+        '
+        Me.s13.AccessibleName = "4f"
+        Me.s13.AutoSize = True
+        Me.s13.Location = New System.Drawing.Point(451, 89)
+        Me.s13.Name = "s13"
+        Me.s13.Size = New System.Drawing.Size(13, 13)
+        Me.s13.TabIndex = 26
+        Me.s13.Tag = "14"
+        Me.s13.Text = "0"
+        '
+        's17
+        '
+        Me.s17.AccessibleName = "5f"
+        Me.s17.AutoSize = True
+        Me.s17.Location = New System.Drawing.Point(451, 118)
+        Me.s17.Name = "s17"
+        Me.s17.Size = New System.Drawing.Size(13, 13)
+        Me.s17.TabIndex = 27
+        Me.s17.Tag = "14"
+        Me.s17.Text = "0"
+        '
+        's22
+        '
+        Me.s22.AccessibleName = "6f"
+        Me.s22.AutoSize = True
+        Me.s22.Location = New System.Drawing.Point(451, 146)
+        Me.s22.Name = "s22"
+        Me.s22.Size = New System.Drawing.Size(13, 13)
+        Me.s22.TabIndex = 28
+        Me.s22.Tag = "14"
+        Me.s22.Text = "0"
+        '
+        's21
+        '
+        Me.s21.AccessibleName = "5g"
+        Me.s21.AutoSize = True
+        Me.s21.Location = New System.Drawing.Point(502, 118)
+        Me.s21.Name = "s21"
+        Me.s21.Size = New System.Drawing.Size(13, 13)
+        Me.s21.TabIndex = 29
+        Me.s21.Tag = "18"
+        Me.s21.Text = "0"
+        '
+        'ec
+        '
+        Me.ec.Font = New System.Drawing.Font("Lucida Console", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ec.Location = New System.Drawing.Point(15, 99)
+        Me.ec.Name = "ec"
+        Me.ec.ReadOnly = True
+        Me.ec.Size = New System.Drawing.Size(257, 150)
+        Me.ec.TabIndex = 30
+        Me.ec.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(289, 261)
+        Me.Controls.Add(Me.ec)
+        Me.Controls.Add(Me.s21)
+        Me.Controls.Add(Me.s22)
+        Me.Controls.Add(Me.s17)
+        Me.Controls.Add(Me.s13)
+        Me.Controls.Add(Me.s23)
+        Me.Controls.Add(Me.s18)
+        Me.Controls.Add(Me.s14)
+        Me.Controls.Add(Me.s10)
+        Me.Controls.Add(Me.s7)
+        Me.Controls.Add(Me.s24)
+        Me.Controls.Add(Me.s19)
+        Me.Controls.Add(Me.s15)
+        Me.Controls.Add(Me.s11)
+        Me.Controls.Add(Me.s8)
+        Me.Controls.Add(Me.s5)
+        Me.Controls.Add(Me.s3)
+        Me.Controls.Add(Me.s25)
+        Me.Controls.Add(Me.s20)
+        Me.Controls.Add(Me.s16)
+        Me.Controls.Add(Me.s12)
+        Me.Controls.Add(Me.s9)
+        Me.Controls.Add(Me.s6)
+        Me.Controls.Add(Me.s4)
+        Me.Controls.Add(Me.s2)
+        Me.Controls.Add(Me.s1)
+        Me.Controls.Add(Me.ok)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.no_e)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Tag = " "
+        Me.Text = "Electron Configuration"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents no_e As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents ok As System.Windows.Forms.Button
+    Friend WithEvents s1 As System.Windows.Forms.Label
+    Friend WithEvents s2 As System.Windows.Forms.Label
+    Friend WithEvents s4 As System.Windows.Forms.Label
+    Friend WithEvents s6 As System.Windows.Forms.Label
+    Friend WithEvents s9 As System.Windows.Forms.Label
+    Friend WithEvents s12 As System.Windows.Forms.Label
+    Friend WithEvents s16 As System.Windows.Forms.Label
+    Friend WithEvents s20 As System.Windows.Forms.Label
+    Friend WithEvents s25 As System.Windows.Forms.Label
+    Friend WithEvents s3 As System.Windows.Forms.Label
+    Friend WithEvents s5 As System.Windows.Forms.Label
+    Friend WithEvents s8 As System.Windows.Forms.Label
+    Friend WithEvents s11 As System.Windows.Forms.Label
+    Friend WithEvents s15 As System.Windows.Forms.Label
+    Friend WithEvents s19 As System.Windows.Forms.Label
+    Friend WithEvents s24 As System.Windows.Forms.Label
+    Friend WithEvents s7 As System.Windows.Forms.Label
+    Friend WithEvents s10 As System.Windows.Forms.Label
+    Friend WithEvents s14 As System.Windows.Forms.Label
+    Friend WithEvents s18 As System.Windows.Forms.Label
+    Friend WithEvents s23 As System.Windows.Forms.Label
+    Friend WithEvents s13 As System.Windows.Forms.Label
+    Friend WithEvents s17 As System.Windows.Forms.Label
+    Friend WithEvents s22 As System.Windows.Forms.Label
+    Friend WithEvents s21 As System.Windows.Forms.Label
+    Friend WithEvents ec As System.Windows.Forms.RichTextBox
+
+End Class
---
+++ VB Electron Configuration/Form1.vb
@@ -0,0 +1,79 @@
+﻿Public Class Form1
+
+    Dim rem_e As Integer
+
+    Public Sub clean()
+        Dim i As Integer = 1
+        Do While i < 25
+            For Each obj As Object In Me.Controls
+                If obj.GetType Is GetType(Label) Then
+                    Dim shell = DirectCast(obj, Label)
+                    If shell.Name = "s" & i Then
+                        shell.Text = "0"
+                        i += 1
+                    End If
+                End If
+            Next
+        Loop
+    End Sub
+
+    Public Sub Compute()
+        clean()
+        Try
+            rem_e = Val(no_e.Text)
+            If Val(no_e.Text) <= 170 Then
+                Dim i As Integer = 1
+                Do Until rem_e = 0
+                    For Each obj As Object In Me.Controls
+                        If obj.GetType Is GetType(Label) Then
+                            Dim shell = DirectCast(obj, Label)
+                            If shell.Name = "s" & i Then
+                                If shell.Tag < rem_e Then
+                                    shell.Text = shell.Tag
+                                    rem_e -= shell.Tag
+                                Else
+                                    shell.Text = rem_e
+                                    rem_e = 0
+                                End If
+                                i += 1
+                            End If
+                        End If
+                    Next
+                Loop
+            Else
+                MsgBox("Please enter a number less than or equal to 170 only", MsgBoxStyle.OkOnly, "Electron Configuration")
+            End If
+        Catch ex As Exception
+            MsgBox("Please enter some number in the Textbox", MsgBoxStyle.OkOnly, "Electron Configuration")
+        End Try
+    End Sub
+
+    Public Sub WriteShell(ByVal b As String, ByVal n As Integer)
+        ec.SelectedText = b
+        ec.SelectionCharOffset = 10
+        ec.SelectedText = n
+        ec.SelectionCharOffset = 0
+        ec.AppendText(" ")
+    End Sub
+
+    Private Sub ok_Click(sender As Object, e As EventArgs) Handles ok.Click
+        Compute()
+        ec.Clear()
+        Dim i As Integer = 1
+        Do Until i = 25
+            For Each obj As Object In Me.Controls
+                If obj.GetType Is GetType(Label) Then
+                    Dim shell = DirectCast(obj, Label)
+                    If shell.Name = "s" & i Then
+                        If shell.Text = 0 Then
+                            Exit Do
+                        Else
+                            WriteShell(shell.AccessibleName, shell.Text)
+                        End If
+                        i += 1
+                    End If
+                End If
+            Next
+        Loop
+    End Sub
+End Class
